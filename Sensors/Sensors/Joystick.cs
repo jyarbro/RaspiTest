0db6cd5724ded2c8efca24e4874a57ec19b112f6
James Yarbro <yarbro@outlook.com>
2016-09-25 11:26:13 +0200

    started working on getting the joystick working. output isn't even remotely accurate but it does flux when stick is moved.

--- Sensors/Sensors/Joystick.cs
+++ Sensors/Sensors/Joystick.cs
@@ -10,10 +10,14 @@ using Windows.Devices.Gpio;
 
 namespace Sensors {
 	internal class Joystick : IDisposable {
-		const int PIN_LED_LEFT = 17;
-		const int PIN_LED_MID = 18;
-		const int PIN_LED_RIGHT = 19;
-		const int PIN_BUTTON = 20;
+		const int PIN_BUTTON = 4;
+		const int PIN_LED_LEFT = 19;
+		const int PIN_LED_MID = 20;
+		const int PIN_LED_RIGHT = 21;
+		const int PIN_LED_BUTTON = 5;
+
+		const int CHAN_X = 1;
+		const int CHAN_Y = 2;
 
 		IAdcControllerProvider Adc { get; set; }
 		GpioController GpioController { get; set; }
@@ -23,9 +27,11 @@ namespace Sensors {
 		GpioPin LedLeft;
 		GpioPin LedMid;
 		GpioPin LedRight;
+		GpioPin LedButton;
 
 		double X;
 		double Y;
+		bool B;
 
 		public async Task InitializeAsync() {
 			var adcManager = new AdcProviderManager();
@@ -37,8 +43,10 @@ namespace Sensors {
 			var adcControllers = await adcManager.GetControllersAsync();
 			var adcController = adcControllers[0];
 
-			Stick.XChannel = adcControllers[0].OpenChannel(0);
-			Stick.YChannel = adcControllers[0].OpenChannel(1);
+			GpioController = GpioController.GetDefault();
+
+			Stick.XChannel = adcControllers[0].OpenChannel(CHAN_X);
+			Stick.YChannel = adcControllers[0].OpenChannel(CHAN_Y);
 			Stick.ButtonPin = GpioController.OpenPin(PIN_BUTTON);
 			Stick.ReadingChanged += Thumbstick_ReadingChanged;
 
@@ -51,17 +59,17 @@ namespace Sensors {
 
 		void Thumbstick_ReadingChanged(IThumbstick sender, ThumbstickReadingChangedEventArgs args) {
 			X = args.Reading.XAxis;
-			Y = args.Reading.XAxis;
+			Y = args.Reading.YAxis;
+			B = args.Reading.IsPressed;
 
 			UpdateLeds();
 		}
 
 		void InitializeLeds() {
-			GpioController = GpioController.GetDefault();
-
 			LedLeft = GpioController.OpenPin(PIN_LED_LEFT);
 			LedMid = GpioController.OpenPin(PIN_LED_MID);
 			LedRight = GpioController.OpenPin(PIN_LED_RIGHT);
+			LedButton = GpioController.OpenPin(PIN_LED_BUTTON);
 
 			if (LedLeft.GetDriveMode() != GpioPinDriveMode.Output)
 				LedLeft.SetDriveMode(GpioPinDriveMode.Output);
@@ -71,6 +79,9 @@ namespace Sensors {
 
 			if (LedRight.GetDriveMode() != GpioPinDriveMode.Output)
 				LedRight.SetDriveMode(GpioPinDriveMode.Output);
+
+			if (LedButton.GetDriveMode() != GpioPinDriveMode.Output)
+				LedButton.SetDriveMode(GpioPinDriveMode.Output);
 		}
 
 		void UpdateLeds() {
@@ -91,6 +102,11 @@ namespace Sensors {
 				LedMid.Write(GpioPinValue.High);
 				LedRight.Write(GpioPinValue.Low);
 			}
+
+			if (B)
+				LedButton.Write(GpioPinValue.High);
+			else
+				LedButton.Write(GpioPinValue.Low);
 		}
 	}
 }
